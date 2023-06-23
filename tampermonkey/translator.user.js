f76df345702b2741711a315bd8dea774bf754b00
github-laziji <c_smtp@163.com>
2023-06-24 00:49:33 +0800

    up

--- tampermonkey/translator.user.js
+++ tampermonkey/translator.user.js
@@ -12,6 +12,101 @@
 
 (function () {
 
+    class BingTranslator {
+        getName() {
+            return "Bing";
+        }
+        getAuth() {
+            return new Promise((resolve, reject) => {
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url: "https://cn.bing.com/translator",
+                    anonymous: true,
+                    nocache: true,
+                    onload: (e) => {
+                        let html = e.responseText;
+                        let data = html.match(/params_AbusePreventionHelper\s*=\s*\[\s*(\d+)\s*,\s*"([^"]+)"\s*,\s*(\d+)\s*]/);
+                        let auth = { key: Number(data[1]), token: data[2], timeout: Number(data[3]) };
+                        resolve(auth);
+                    },
+                    onerror: (e) => {
+                        console.log("BingTranslator.getAuth() error", e);
+                        reject();
+                    }
+                });
+            });
+        }
+        translation(auth, texts) {
+            let { key, token } = auth;
+            let promise = Promise.resolve({});
+            let subTextLen = 0;
+            let subTexts = [];
+            for (let i = 0; i < texts.length; i++) {
+                subTexts.push(texts[i]);
+                subTextLen += texts[i].length;
+                if (i == texts.length - 1 || subTextLen + texts[i + 1].length > 4000) {
+                    let tempTexts = subTexts;
+                    subTexts = [];
+                    subTextLen = 0;
+                    promise = promise.then((result) => {
+                        return new Promise((resolve, reject) => {
+                            GM_xmlhttpRequest({
+                                method: "POST",
+                                url: "https://cn.bing.com/ttranslatev3?isVertical=1&&IG=2CB2C389B2FD46AE96301F20ACD168D1&IID=translator.5027",
+                                data: `token=${token}&key=${key}&text=${encodeURIComponent(tempTexts.join("🍕"))}&fromLang=auto-detect&to=zh-Hans&tryFetchingGenderDebiasedTranslations=true`,
+                                headers: {
+                                    "content-type": "application/x-www-form-urlencoded"
+                                },
+                                anonymous: true,
+                                nocache: true,
+                                onload: (e) => {
+                                    let transText = eval(e.responseText)[0].translations[0].text;
+                                    let transArr = transText.split("🍕");
+                                    tempTexts.forEach((v, i) => result[v] = transArr[i]);
+                                    resolve(result);
+                                },
+                                onerror: (e) => {
+                                    console.log("BingTranslator.translation() error", e);
+                                    reject();
+                                }
+                            });
+                        });
+                    });
+                }
+            }
+            return promise;
+        }
+    }
+
+
+
+    
+
+    const TRANSLATOR_CLASSES = [BingTranslator];
+    const TRANSLATOR_INSTANCE_MAPPING = {};
+    TRANSLATOR_CLASSES.forEach(translatorClass => {
+        let ins = new translatorClass();
+        TRANSLATOR_INSTANCE_MAPPING[ins.getName()] = ins;
+    });
+
+    let instance;
+    if (GM_getValue("ACTIVE_INSTANCE_NAME")) {
+        instance = TRANSLATOR_INSTANCE_MAPPING[GM_getValue("ACTIVE_INSTANCE_NAME")];
+    } else {
+        for (let name in TRANSLATOR_INSTANCE_MAPPING) {
+            instance = TRANSLATOR_INSTANCE_MAPPING[name];
+            GM_setValue("ACTIVE_INSTANCE_NAME", name);
+        }
+    }
+
+    let auths;
+    if (GM_getValue("AUTHS")) {
+        auths = JSON.parse(GM_getValue("AUTHS"));
+    } else {
+        auths = {};
+        GM_setValue("AUTHS", auths);
+    }
+
     const getSelectedNodes = function () {
         let range = window.getSelection().getRangeAt(0);
         let selected = range.commonAncestorContainer;
@@ -38,113 +133,88 @@
         return childNodes;
     }
 
-    let p = new Promise((r, j) => {
-        let authStr = GM_getValue("auth");
-        if (authStr) {
-            let auth = JSON.parse(authStr);
-            if (new Date().getTime() - auth.time < auth.timeout * 0.8) {
-                r(auth);
-                return;
-            }
+    let promise = new Promise((resolve, reject) => {
+        let auth = auths[instance.getName()];
+        if (auth && new Date().getTime() - auth.time < auth.timeout * 0.8) {
+            resolve(auth);
+            console.log(auth)
+            return;
         }
-        GM_xmlhttpRequest({
-            method: "GET",
-            url: "https://cn.bing.com/translator",
-            anonymous: true,
-            nocache: true,
-            onload: (e) => {
-                let html = e.responseText;
-                let data = html.match(/params_AbusePreventionHelper\s*=\s*\[\s*(\d+)\s*,\s*"([^"]+)"\s*,\s*(\d+)\s*]/);
-                let auth = { key: Number(data[1]), token: data[2], timeout: Number(data[3]), time: new Date().getTime() };
-                GM_setValue("auth", JSON.stringify(auth));
-                r(auth);
-            }
+        return instance.getAuth().then(auth => {
+            auth.time = new Date().getTime();
+            auths[instance.getName()] = auth;
+            GM_setValue("AUTHS", JSON.stringify(auths));
+            console.log(auths)
+            return auth;
         });
     });
 
     document.addEventListener('keydown', (e) => {
         if (e.ctrlKey && e.code === "KeyQ") {
             let textMap = {};
-            let textArr = [];
+            let texts = [];
+            console.log(getSelectedNodes())
             getSelectedNodes().forEach(t => {
+                console.log(t)
                 if (!/[a-zA-Z]{2,}/.test(t.textContent.trim())) {
                     return;
                 }
-                if (t.TRANSLATION_STATUS == "TRANSLATING" || t.TRANSLATION_STATUS == "TRANSLATED") {
+                console.log(1, t.TRANSLATION_STATUS, !t.TRANSLATION_STATUS && t.TRANSLATION_STATUS != "INIT")
+                if (t.TRANSLATION_STATUS && t.TRANSLATION_STATUS != "INIT") {
                     return;
                 }
+                console.log(2)
                 if (t.TRANSLATION_TEXT) {
                     t.TRANSLATION_STATUS = "TRANSLATED";
                     t.textContent = t.TRANSLATION_TEXT;
                     return;
                 }
+                console.log(3)
                 t.TRANSLATION_STATUS = "TRANSLATING";
                 if (!textMap[t.textContent]) {
                     textMap[t.textContent] = [];
-                    textArr.push(t.textContent);
+                    texts.push(t.textContent);
                 }
+                console.log(4)
                 textMap[t.textContent].push(t);
                 return;
             });
-            console.log(textArr)
-            if (!textArr.length) {
+            console.log(texts)
+            if (!texts.length) {
                 return;
             }
-            let subTextLen = 0;
-            let subTextArr = [];
-            for (let i = 0; i < textArr.length; i++) {
-                subTextArr.push(textArr[i]);
-                subTextLen += textArr[i].length;
-                if (i == textArr.length - 1 || subTextLen + textArr[i + 1].length > 4000) {
-                    let tempArr = subTextArr;
-                    subTextArr = [];
-                    subTextLen = 0;
-                    p = p.then((auth) => {
-                        return new Promise((r, j) => {
-                            GM_xmlhttpRequest({
-                                method: "POST",
-                                url: "https://cn.bing.com/ttranslatev3?isVertical=1&&IG=2CB2C389B2FD46AE96301F20ACD168D1&IID=translator.5027",
-                                data: `token=${auth.token}&key=${auth.key}&text=${encodeURIComponent(tempArr.join("🍕"))}&fromLang=auto-detect&to=zh-Hans&tryFetchingGenderDebiasedTranslations=true`,
-                                headers: {
-                                    "content-type": "application/x-www-form-urlencoded"
-                                },
-                                anonymous: true,
-                                nocache: true,
-                                onload: (e) => {
-                                    let transText = eval(e.responseText)[0].translations[0].text;
-                                    let transArr = transText.split("🍕");
-                                    transArr.forEach((v, i) => {
-                                        textMap[tempArr[i]].forEach(t => {
-                                            t.TRANSLATION_TEXT = v;
-                                            if (t.TRANSLATION_STATUS == "TRANSLATING") {
-                                                t.ORIGIN_TEXT = t.textContent;
-                                                t.TRANSLATION_STATUS = "TRANSLATED";
-                                                t.textContent = v;
-                                            }
-                                        });
-                                    });
-                                    r(auth);
+            promise = promise.then(auth => {
+                return instance.translation(auth, texts).then(result => {
+                    for (let text in result) {
+                        let v = result[text];
+                        textMap[text].forEach(t => {
+                            if (v) {
+                                t.TRANSLATION_TEXT = v;
+                                if (t.TRANSLATION_STATUS == "TRANSLATING") {
+                                    t.ORIGIN_TEXT = t.textContent;
+                                    t.TRANSLATION_STATUS = "TRANSLATED";
+                                    t.textContent = v;
                                 }
-                            });
-                        });
-                    });
-                }
-            }
+                            } else {
+                                if (t.TRANSLATION_STATUS == "TRANSLATING") {
+                                    t.TRANSLATION_STATUS = "INIT";
+                                }
+                            }
+                        })
+                    }
+                    return auth;
+                });
+            });
         }
     });
 
     document.addEventListener('keydown', (e) => {
         if (e.ctrlKey && e.code === "KeyW") {
             getSelectedNodes().forEach(t => {
-                if (t.TRANSLATION_STATUS == "TRANSLATING") {
-                    t.TRANSLATION_STATUS = "DISCONTINUED";
-                    return;
-                }
                 if (t.TRANSLATION_STATUS == "TRANSLATED") {
-                    t.TRANSLATION_STATUS = "INIT";
                     t.textContent = t.ORIGIN_TEXT;
-                    return;
                 }
+                t.TRANSLATION_STATUS = "INIT";
             });
         }
     });
